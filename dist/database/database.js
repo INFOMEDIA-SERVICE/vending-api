72bb133d7133819838e55aaf72522a98137cbb63
Andres Carrillo <andresdavidcm24@gmail.com>
2021-01-07 11:38:22 -0500

    IDK what is happening

--- dist/database/database.js
+++ dist/database/database.js
@@ -3,12 +3,12 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.database = void 0;
 const pg_1 = require("pg");
 // POSTGRESS_USER='postgres'
-// POSTGRESS_DATABASE='vendings'
+// POSTGRESS_DATABASE='smartinfo_vending'
 // POSTGRESS_HOST='localhost'
 // POSTGRESS_PASSWORD='37375930'
 exports.database = new pg_1.Pool({
     user: (process.env.ENV == 'D') ? 'postgres' : process.env.POSTGRESS_USER,
-    database: (process.env.ENV == 'D') ? 'vendings' : process.env.POSTGRESS_DATABASE,
+    database: (process.env.ENV == 'D') ? 'smartinfo_vending' : process.env.POSTGRESS_DATABASE,
     host: (process.env.ENV == 'D') ? 'localhost' : process.env.POSTGRESS_HOST,
     password: (process.env.ENV == 'D') ? '37375930' : process.env.POSTGRESS_PASSWORD
 });
--- src/database/database.ts
+++ src/database/database.ts
@@ -2,13 +2,13 @@ import { Pool } from 'pg';
 
 
 // POSTGRESS_USER='postgres'
-// POSTGRESS_DATABASE='vendings'
+// POSTGRESS_DATABASE='smartinfo_vending'
 // POSTGRESS_HOST='localhost'
 // POSTGRESS_PASSWORD='37375930'
 
 export const database: Pool = new Pool({
     user: (process.env.ENV == 'D') ? 'postgres' : process.env.POSTGRESS_USER,
-    database: (process.env.ENV == 'D') ? 'vendings' : process.env.POSTGRESS_DATABASE,
+    database: (process.env.ENV == 'D') ? 'smartinfo_vending' : process.env.POSTGRESS_DATABASE,
     host: (process.env.ENV == 'D') ? 'localhost' : process.env.POSTGRESS_HOST,
     password: (process.env.ENV == 'D') ? '37375930' : process.env.POSTGRESS_PASSWORD
 });
